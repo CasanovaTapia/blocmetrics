a5ede93018d00a89a20ceecd2fc0144799cd5168
CasanovaTapia <ching@casanovatapia.com>
2014-11-03 10:53:34 +0800

    Modified event model methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,20 +3,11 @@ class Event < ActiveRecord::Base
   cattr_accessor :user_events
 
   def self.unique_ips
-    unique_ips = Event.all.collect(&:ip_address).uniq
+    unique_ips = Event.all.select(&:ip_address).uniq
   end
 
-  def self.unique_emails
-    unique_emails = Event.all.collect(&:property_2).uniq
-  end
-
-  def self.visits
-    visits = []
-
-    unique_ips.each_with_index do |v, i|
-      visits << Event.where(ip_address: unique_ips[i].count)
-    end
-    visits
+  def self.unique_users
+    unique_users = Event.all.select(&:property_2).uniq
   end
 
   # Collect events by current user
